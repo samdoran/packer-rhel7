bec2de2edc4768a1aa89e2364e56d46bdf6c7987
Sam Doran <sdoran@ansible.com>
2016-06-09 17:45:29 -0400

    Clean yum cache

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -2,6 +2,7 @@
 
 # Remove Ansible and its dependencies.
 yum -y remove ansible
+yum clean all
 
 # Zero out the rest of the free space using dd, then delete the written file.
 dd if=/dev/zero of=/EMPTY bs=1M
