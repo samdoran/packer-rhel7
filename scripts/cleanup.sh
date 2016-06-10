2b488846d40b9eecdb1142595377808d4c77c26c
Sam Doran <sdoran@ansible.com>
2016-06-10 12:03:24 -0400

    Reboot and cleanup old kernels

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -2,7 +2,12 @@
 
 # Remove Ansible and its dependencies.
 yum -y remove ansible
+
+# Clean yum cache
 yum clean all
+
+# Delete old kernels
+package-cleanup -C -y --oldkernels --count=1
 
 # Zero out the rest of the free space using dd, then delete the written file.
 dd if=/dev/zero of=/EMPTY bs=1M
