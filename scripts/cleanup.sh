c02c8fc8c1426ddb1769cc217e2251702f8fd950
Sam Doran <sdoran@redhat.com>
2016-11-17 10:54:16 -0500

    Adjust cleanup.sh to ensure yum cache is removed
    Alse remove ansible dependencies

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 # Remove Ansible and its dependencies.
-yum -y remove ansible
-
-# Clean yum cache
-yum clean all
+yum -C -y autoremove ansible
 
 # Delete old kernels
 package-cleanup -C -y --oldkernels --count=1
 
+# Clean yum cache
+echo "Removing yum cache"
+rm -rf /var/cache/yum/*
+
 # Zero out the rest of the free space using dd, then delete the written file.
+echo "Reclaming free space on disk"
 dd if=/dev/zero of=/EMPTY bs=1M
 rm -f /EMPTY
 
