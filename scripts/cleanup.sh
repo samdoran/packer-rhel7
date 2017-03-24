96667677f003d944d22a5bd58874109b98d8a1b1
Sam Doran <sdoran@redhat.com>
2017-03-24 18:03:44 -0400

    Cleanup backgrounds and firmware to save space

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -2,6 +2,7 @@
 
 # Remove Ansible and its dependencies.
 yum -C -y autoremove ansible
+yum -C -y remove kernel-firmware 'iwl*'
 
 # Delete old kernels
 package-cleanup -C -y --oldkernels --count=1
@@ -12,6 +13,7 @@ rm -rf /var/cache/yum/*
 rm -rf /usr/share/man/*
 rm -rf /usr/share/info/*
 rm -rf /usr/share/doc/*
+rm -rf /usr/share/backgrounds/*
 rpm --rebuilddb
 
 # Zero out the rest of the free space using dd, then delete the written file.
