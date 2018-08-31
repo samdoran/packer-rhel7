01709ee90d85046013586942652db37349099a70
Sam Doran <sdoran@redhat.com>
2018-08-31 10:41:44 -0400

    Add vagrant-cloud post processor
    Add vagrant-cloud post processor
    
    Use RHEL repo to install Ansible

--- scripts/install-ansible.sh
+++ scripts/install-ansible.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Install EPEL
-yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+#yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 
 # Install Ansible
-yum --enablerepo=epel-testing -y install ansible
+subscription-manager repos --enable rhel-7-server-ansible-2.6-rpms
+yum -y install ansible
